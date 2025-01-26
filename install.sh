5d788b05038c2098e7ead092c6f7c0480adc5b10
vedantthapa <thapavedant@gmail.com>
2025-01-26 10:36:27 -0400

    fix: add unattended to oh-my-zsh install

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ fi
 # install Oh My Zsh
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
     echo "Installing Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
     echo "Oh My Zsh is already installed."
 fi
