fa92c2449975c74a9cd82674ed4ecbf6097a4df9
vedantthapa <thapavedant@gmail.com>
2025-01-26 11:03:19 -0400

    feat: remove oh-my-zsh installation to let it install via stow and arrange chsh cmds

--- install.sh
+++ install.sh
@@ -27,6 +27,31 @@ else
     echo "Brewfile not found, skipping."
 fi
 
+# install LunarVim
+if [[ ! -d "$HOME/.local/share/lunarvim" ]]; then
+    echo "Installing LunarVim..."
+    LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
+else
+    echo "LunarVim is already installed."
+fi
+
+# symlink configs using stow
+if command -v stow &>/dev/null; then
+    echo "Symlinking configuration files with stow..."
+    stow -R .
+else
+    echo "GNU Stow is not installed. Please install it and run the script again."
+    exit 1
+fi
+
+# setup tmux plugins
+if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
+    echo "Installing tmux plugin manager (tpm)..."
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+    echo "tmux plugin manager (tpm) is already installed."
+fi
+
 # add zsh as a login shell
 if ! grep -q "$(command -v zsh)" /etc/shells; then
     echo "Adding zsh to the list of login shells..."
@@ -39,46 +64,6 @@ if [[ "$SHELL" != "$(command -v zsh)" ]]; then
     sudo chsh -s "$(which zsh)" "$USER"
 else
     echo "zsh is already the default shell."
-fi
-
-# install Oh My Zsh
-if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-    echo "Installing Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-else
-    echo "Oh My Zsh is already installed."
-fi
-
-# install powerlevel10k
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
-# install LunarVim
-if [[ ! -d "$HOME/.local/share/lunarvim" ]]; then
-    echo "Installing LunarVim..."
-    LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
-    export PATH=/home/cloud_user/.local/bin:$PATH
-else
-    echo "LunarVim is already installed."
-fi
-
-# prevent stow conflicts
-rm -rf ~/.zshrc ~/.config/lvim
-
-# symlink configs using stow
-if command -v stow &>/dev/null; then
-    echo "Symlinking configuration files with stow..."
-    stow .
-else
-    echo "GNU Stow is not installed. Please install it and run the script again."
-    exit 1
-fi
-
-# setup tmux plugins
-if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
-    echo "Installing tmux plugin manager (tpm)..."
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-else
-    echo "tmux plugin manager (tpm) is already installed."
 fi
 
 echo "Setup completed successfully!"
